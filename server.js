9e645866bedf2eff0e95b6ae5d5401a43513ee9f
maramshon <mralshon@gmail.com>
2017-11-02 08:27:48 +0200

    chatter box withot refactor

--- server.js
+++ server.js
@@ -36,6 +36,60 @@ app.use(session({
     saveUninitialized: true
 }));
 
+
+
+////////////////////////////
+
+var http = require('http')
+var socketio = require('socket.io');
+
+var server = http.Server(app);
+var websocket = socketio(server);
+
+// var connection=require('./connection/connectionConnection');
+
+server.listen(8000, () => console.log('listening on *:8000'));
+websocket.on('connection', (socket) => {
+  
+  console.log("user connected ================================");
+
+  var sql="select * from msgT;";
+  connection.query(sql,function(err,result){
+          if(err){
+            throw err
+        }
+        console.log("query result =====>",result);
+        websocket.emit('allDataBase', result);
+  }) 
+
+  socket.on('message', (message) => {
+      // debugger;
+      console.log('HHHHHHHHHH ========> ',message);
+      if(!message){
+        return 
+      }
+      var sql="insert into msgT (user,text,date) values ('"+message.user+"','"+message.text+"','"+message.date+"');";
+            connection.query(sql,function(err,result){
+                    if(err){
+                        throw err
+                    }
+                })
+      // websocket.emit('msg', message);
+      var sql="select * from msgT;";
+      connection.query(sql,function(err,result){
+          if(err){
+            throw err
+        }
+        console.log("query result =====>",result);
+        websocket.emit('msg', result);
+  }) 
+  });
+
+  socket.on('disconnect', function(){
+       console.log('user disconnected');
+   });
+});
+////////////////////////////
 //database
 
 var connection = mysql.createConnection({
@@ -49,90 +103,102 @@ connection.connect(function(err) {
     if (err) {
         console.log('errrror');
     }
-
+    // var sql3 =
+    //  'CREATE TABLE msgT (id INT AUTO_INCREMENT PRIMARY KEY, user varchar (50), text varchar(200), date varchar(20));';
+    //  connection.query(sql3, function(err, result){
+    //    if (err){
+    //      throw err;
+    //    }
+    //    console.log('CREATE Chat Box table in mysql');
+    //  });
      console.log("connection connected")
-    //  var sql =
-    //         'CREATE TABLE user (id INT AUTO_INCREMENT PRIMARY KEY, name varchar(255), password varchar(255), email varchar(200), image varchar(255), api varchar(250))';
-    //     connection.query(sql, function(err, result) {
-    //         if (err) {
-    //             throw err;
-    //         }
-    //         console.log('CREATE TABLE usre');
-    //     });
+     // var sql="drop table msgT;"
+     // connection.query(sql, function(err, result) {
+     //   if(err)
+     //   console.log("err")
+     // })
+     // var sql =
+     //        'CREATE TABLE user (id INT AUTO_INCREMENT PRIMARY KEY, name varchar(255), password varchar(255), email varchar(200), image varchar(255), api varchar(250))';
+     //    connection.query(sql, function(err, result) {
+     //        if (err) {
+     //            throw err;
+     //        }
+     //        console.log('CREATE TABLE usre');
+     //    });
 
     //   console.log("connected to connection")
-    //     var sql2 = 
-    //     'CREATE TABLE components (id INT AUTO_INCREMENT PRIMARY KEY, component varchar (200))';
-    //     connection.query(sql2, function(err, result){
-    //         if(err){
-    //             throw err;
-    //         }
-    //         console.log('CREATE TABLE components');
-    //     });
+        // var sql2 = 
+        // 'CREATE TABLE components (id INT AUTO_INCREMENT PRIMARY KEY, component varchar (200))';
+        // connection.query(sql2, function(err, result){
+        //     if(err){
+        //         throw err;
+        //     }
+        //     console.log('CREATE TABLE components');
+        // });
     });
 
 
 
 //signup user
 app.post('/signup',(req,res)=>{
-	console.log("comming data =======>", req.body.user)
-	//checck if user allready exist  
-	var sql="select * from user where name='"+req.body.user.username+"';";
-	connection.query(sql,function(err,result){
-		if(err){
-			throw err
-		}
-		console.log("query result =====>",result);
-		// if exist return exist 
-		if(result.length){
-			res.status(200)
-			return res.send(JSON.stringify("exist"));
-		}
+  console.log("comming data =======>", req.body.user)
+  //checck if user allready exist  
+  var sql="select * from user where name='"+req.body.user.username+"';";
+  connection.query(sql,function(err,result){
+    if(err){
+      throw err
+    }
+    console.log("query result =====>",result);
+    // if exist return exist 
+    if(result.length){
+      res.status(200)
+      return res.send(JSON.stringify("exist"));
+    }
 
-		bcrypt.hash(req.body.user.password, null, null, function(err, hash){
-		//else insert it into database
-		var sql="insert into user (name,password,image) values ('"+req.body.user.username+"','"+hash+"','"+req.body.user.image+"');";
-		connection.query(sql,function(err,result){
-			if(err){
-				throw err
-			}
-			res.status(200);
-			return res.send(JSON.stringify("inserted"));
-		})
-		})
-	})
+    bcrypt.hash(req.body.user.password, null, null, function(err, hash){
+    //else insert it into database
+    var sql="insert into user (name,password,image) values ('"+req.body.user.username+"','"+hash+"','"+req.body.user.image+"');";
+    connection.query(sql,function(err,result){
+      if(err){
+        throw err
+      }
+      res.status(200);
+      return res.send(JSON.stringify("inserted"));
+    })
+    })
+  })
 });
 //login user 
 app.post('/login',(req,res)=>{
-	console.log(req.body)
-	//check if username exist
-	var sql="select * from user where name='"+req.body.user.username+"';"
-	connection.query(sql,(err,result)=>{
-		console.log("the result is ====> ",result)
-		if(err)
-			console.log("errrrror")
-		if(result.length){
-			//check password
-			console.log(req.body.user.password)
-			console.log("resullllt",result[0].password)
+  console.log(req.body)
+  //check if username exist
+  var sql="select * from user where name='"+req.body.user.username+"';"
+  connection.query(sql,(err,result)=>{
+    console.log("the result is ====> ",result)
+    if(err)
+      console.log("errrrror")
+    if(result.length){
+      //check password
+      console.log(req.body.user.password)
+      console.log("resullllt",result[0].password)
 
-			bcrypt.compare(req.body.user.password, result[0].password, function(err, hash){
+      bcrypt.compare(req.body.user.password, result[0].password, function(err, hash){
 
-			if(hash){
-				//create session 
-				req.session.username=result[0].name;
-				req.session.password=result[0].password;
-				console.log("the session is ===> ",req.session)
-				return res.send(JSON.stringify("done"));
-			}else{
-				return res.send(JSON.stringify("not exist"));
-			}
-		})
+      if(hash){
+        //create session 
+        req.session.username=result[0].name;
+        req.session.password=result[0].password;
+        console.log("the session is ===> ",req.session)
+        return res.send(JSON.stringify("done"));
+      }else{
+        return res.send(JSON.stringify("not exist"));
+      }
+    })
 
-		}else{
-			return res.send(JSON.stringify("not exist"));
-		}
-	})
+    }else{
+      return res.send(JSON.stringify("not exist"));
+    }
+  })
 })
 //ligout
 app.get('/logout', function(req,res){
@@ -143,47 +209,47 @@ app.get('/logout', function(req,res){
 })
 // return user info
 app.get('/user',(req,res) =>{
-	var sql="select * from user where name='"+req.session.username+"';"
-	connection.query(sql,(err,result)=>{
-		if(err){
-			throw err;
-		}
-		return res.send(JSON.stringify(result[0]))
-	})
-	
+  var sql="select * from user where name='"+req.session.username+"';"
+  connection.query(sql,(err,result)=>{
+    if(err){
+      throw err;
+    }
+    return res.send(JSON.stringify(result[0]))
+  })
+  
 })
 //update the image in database
 app.post('/SetNewImage',(req,res) =>{
-	var name=req.body.user.name;
-	var image=req.body.user.image;
-	var sql="update user set image='"+image+"' where name='"+name+"';"
-	connection.query(sql,(err,result)=>{
-		if(err){
-			throw err;
-		}
-		console.log("image updated");
-		res.send();
-	})
+  var name=req.body.user.name;
+  var image=req.body.user.image;
+  var sql="update user set image='"+image+"' where name='"+name+"';"
+  connection.query(sql,(err,result)=>{
+    if(err){
+      throw err;
+    }
+    console.log("image updated");
+    res.send();
+  })
 })
 //update the user name in database 
 app.post('/SetName',(req,res) =>{
-	var name=req.body.user.name;
-	var sql="update user set name='"+name+"' where name='"+req.session.username+"';"
-	connection.query(sql,(err,result)=>{
-		if(err){
-			throw err;
-		}
-		console.log("usename updated");
-		req.session.username=name
-		res.send();
-	})
+  var name=req.body.user.name;
+  var sql="update user set name='"+name+"' where name='"+req.session.username+"';"
+  connection.query(sql,(err,result)=>{
+    if(err){
+      throw err;
+    }
+    console.log("usename updated");
+    req.session.username=name
+    res.send();
+  })
 })
 
 //specify port number
-var port = process.env.PORT||8000;
-//run the server 
-app.listen(port,(err) =>{
-	if(err)
-		throw err
-	console.log('listening on 8000')
-})
+// var port = process.env.PORT||8000;
+// //run the server 
+// app.listen(port,(err) =>{
+//  if(err)
+//    throw err
+//  console.log('listening on 8000')
+// })
--- src/routes/ChatBox.js
+++ src/routes/ChatBox.js
@@ -1,74 +1,480 @@
 import React from 'react';
-import {
-    StyleSheet,
-    View,
-    Text,
-    KeyboardAvoidingView,
-    Image
-
-    
-} from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, Alert, FlatList, StyleSheet, } from 'react-native';
+import SocketIOClient from 'socket.io-client';
+// import SocketIOClient from 'https://code.jquery.com/jquery-1.11.1.js'
 import { Icon} from 'react-native-elements'; 
 
 export default class ChatBox extends React.Component {
-           static navigationOptions={
-        header: null,     
-        tabBarLabel:'ChatBox',
-        tabBarIcon:()=> {
-            return <Icon name="chat" size={25} color={"white"}/>
+ static navigationOptions={
+   header: null,    
+   tabBarLabel:'ChatBox',
+   tabBarIcon:()=> {
+     return <Icon name="chat" size={25} color={"white"}/>
+   }
+ }
+  constructor(props){
+    super(props)
+    this.state={
+      arr:[{user:'maram', text:'please work', date:'for ever'}],
+      msg:'========'
+    }
+    this.socket = SocketIOClient('http://192.168.8.119:8000');
+
+    this.socket.on('allDataBase',(data)=> {
+      this.showArr(data); 
+    })
+
+    this.socket.on('msg', (data)=> {
+      this.showArr(data);
+    })
+
+  }
+
+  // function showArr(data);
+  showArr(data){
+    if(typeof data === 'object'){
+      // for(var i=0; i<data.length; i++){
+      //   this.setState((pre) => {
+      //     return {
+      //       arr: pre.arr.concat(
+      //         {
+      //           user: data[i].user,
+      //           text: data[i].text,
+      //           date: data[i].date
+      //         }
+      //       )
+      //     }
+      //   }) 
+      // }
+      this.setState((pre)=> {
+        return {
+          arr:data
         }
+      })
     }
-    constructor(props) {
-        super(props);
-
+    else{
+      this.setState((pre) => {
+        // return {
+          // arr: pre.arr.concat(
+          //   {
+          //     user: data.user,
+          //     text: data.text,
+          //     date: data.date
+          //   }
+          // )
+          arr: pre.arr.concat(data)
+        // }
+      }) 
     }
+  }
+ 
+  changeMsg(data){ 
+    this.setState({
+      msg: data
+    }) 
+  }
+  
+  send(data){
+    this.socket.emit('message', {user: 'user1', text: data, date:'2017/11'})
+  }
 
-
-
-    render() {
-        return (
-            <KeyboardAvoidingView behavior="padding" style={styles.container}>
-                <View style={styles.logoContainer}>
-                    <Image
-                        style={styles.logo}
-                        source={require('./Smart.png')}
-                    />
-                </View>
-                <Text style={styles.header}>
-                    {' '}
-                    Welcome to chatBox
-                </Text>
-
-            
-            </KeyboardAvoidingView>
-            
-        )
-    }
+  render(){
+    return(
+      <View>
+        <Text>{this.state.msg}</Text>
+        <Text>Hello in chat page</Text>
+        <TextInput
+            placeholder = "Type here ..."
+            onChangeText =  {(text) => this.changeMsg(text)} //{(text) => {this.setState({msg:text})}}  
+            value = {this.state.msg}
+        />
+        <TouchableOpacity
+            onPress={()=>{
+              this.send(this.state.msg)   // function send and add to it the msg which is from TextInput
+            }
+            }
+            >
+            <Text>Send</Text>
+        </TouchableOpacity>
+        <FlatList
+            data={this.state.arr}
+            renderItem={({item}) => <Text style={styles.item}>{item.user + ': '+item.text}</Text>}
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#87CEFA',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
+ container: {
+  flex: 1,
+  paddingTop: 22
+},
+item: {
+ padding: 10,
+ fontSize: 18,
+ height: 44,
+},
+})
 
-    logoContainer: {
-        alignItems: 'center',
-        flexGrow: 1,
-        justifyContent: 'center'
-    },
 
-    logo: {
-        width: 200,
-        height: 200
-    },
-    header: {
-        color: '#fff',
-        marginTop: 5,
-        textAlign: 'center',
-        opacity: 0.8,
-        fontWeight: '700'
-    }
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { View, Text, TouchableOpacity, TextInput, Alert, FlatList, StyleSheet, } from 'react-native';
+// import SocketIOClient from 'socket.io-client';
+// import SocketIOClient from 'https://code.jquery.com/jquery-1.11.1.js'
+// import { Icon} from 'react-native-elements'; 
+
+// export default class ChatBox extends React.Component {
+//  static navigationOptions={
+//    header: null,    
+//    tabBarLabel:'ChatBox',
+//    tabBarIcon:()=> {
+//      return <Icon name="chat" size={25} color={"white"}/>
+//    }
+//  }
+
+//  constructor(props){
+//    super(props);
+//    this.state={
+//      msg:'',
+//      arr:[],
+//      tag:{}
+//    }
+
+//    //this.send=this.send.bind(this);
+
+//    this.socket = SocketIOClient('http://192.168.8.119:8000');
+//    // this.socket.on('firstTime', (data) => {
+//    //   {this.setState({arr:data})}
+//    // })
+//    this.socket.on('msg', (data) => {
+//      console.log(data);
+//      {this.setState({tag:data})}
+//      this.insertTag(this.state.tag)
+//      // Alert.alert(data)
+//      // this.setState({data:data})
+//    });
+//  }
+
+//  send(msg){
+//    console.log({user: "user", text: msg, date: new Date()})
+//    this.socket.emit('message', {user: "user", text: msg, date: "new Date()"});
+//  }
+
+//  insertTag = (tag) =>{
+//        this.setState({
+//            arr: this.state.arr.concat(tag)
+//        })
+//  }
+
+//  // onPress = {() => {this.insertTag(tag) }}
+
+//  render(){
+//    var user = "Frist Client"
+//    return (
+//      <View>
+//        <Text>{user}</Text>
+//        <Text>Hello in chat page</Text>
+//        <TextInput
+//            placeholder="Type here ..."
+//            onChangeText={(text) => {this.setState({msg:text})}}  
+//            value={this.state.msg}/>
+//        <TouchableOpacity
+//            onPress={this.send(this.state.msg)}>
+//            <Text>Send</Text>
+//        </TouchableOpacity>
+//        <FlatList
+//          data={this.state.arr}
+//          renderItem={({item}) => <Text style={styles.item}>{item.user + ': '+item.text}</Text>}
+//        />
+//      </View>
+//    )
+//  }
+// }
+
+// const styles = StyleSheet.create({
+//  container: {
+//   flex: 1,
+//   paddingTop: 22
+//  },
+//  item: {
+//    padding: 10,
+//    fontSize: 18,
+//    height: 44,
+//  },
+// })
+
+
+
+
+
+// import React from 'react';
+// import { View, Text, TouchableOpacity, TextInput,Alert } from 'react-native';
+// import SocketIOClient from 'socket.io-client';
+// // import SocketIOClient from 'https://code.jquery.com/jquery-1.11.1.js'
+// import { Icon} from 'react-native-elements'; 
+
+// // export default class App extends React.Component {
+// export default class ChatBox extends React.Component {
+//           static navigationOptions={
+//        header: null,    
+//        tabBarLabel:'ChatBox',
+//        tabBarIcon:()=> {
+//            return <Icon name="chat" size={25} color={"white"}/>
+//        }
+//    }
+// constructor(props){
+//   super(props);
+//   this.state={
+//     msg:'',
+//     x: 'no response until now',
+//     data:""
+//   }
+//   this.send=this.send.bind(this);
+//   // this.recieve=this.recieve.bind(this);
+
+//  this.socket = SocketIOClient('http://192.168.8.119:8000');
+//  this.socket.on("kamel",(data)=>{
+//     console.log("hahahahahahah")
+//  })
+//   // this.socket.on('message', this.onReceivedMessage);
+//   this.socket.on('msg', (data) => {
+//     console.log(data);
+//     Alert.alert(data)
+//    // this.setState({data:data})
+//   });
+// }
+
+// send(msg){
+//   // Alert.alert(msg);
+//   this.socket.emit('message', msg);
+// }
+// // recieve(msg){
+// //   console.log(msg)
+// // }
+// render(){
+//   return (
+//     <View>
+//       <Text>{this.state.x}</Text>
+//       <Text>Hello in chat page</Text>
+//       <TextInput
+//           placeholder="Type here ..."
+//           onChangeText={(text) => {this.setState({msg:text})}}  
+//          value={this.state.msg}/>
+//       <TouchableOpacity
+//           onPress={this.send(this.state.msg)}>
+//         <Text>Send</Text>
+     
+//       </TouchableOpacity>
+//     </View>
+//   )
+// }
+// }
+
+
+
+
+
+
+// import React from 'react';
+// import { View, Text, TouchableOpacity, TextInput, Alert, FlatList, StyleSheet, } from 'react-native';
+// import SocketIOClient from 'socket.io-client';
+// // import SocketIOClient from 'https://code.jquery.com/jquery-1.11.1.js'
+// import { Icon} from 'react-native-elements'; 
+
+// // export default class App extends React.Component {
+// export default class ChatBox extends React.Component {
+//           static navigationOptions={
+//        header: null,    
+//       tabBarLabel:'ChatBox',
+//        tabBarIcon:()=> {
+//            return <Icon name="chat" size={25} color={"white"}/>
+//        }
+//    }
+//  constructor(props){
+//    super(props);
+//    this.state={
+//      arr:[],
+//      msg: ''
+//    }
+
+//  //  this.send=this.send.bind(this);
+// //   this.insertTag=this.insertTag.bind(this);
+
+//    this.socket = SocketIOClient('http://192.168.8.119:8000');
+//      this.socket.on('allDataBase', (data) => {
+//        for(var i=0; i<data.length; i++){
+//          // {this.setState((pre)=> {return {arr:this.state.arr.concat({user: data[i].user, text: data[i].text, date: data[i].date})}})}
+//           {this.setState((pre)=> {return {arr:pre.arr.concat({user: data[i].user, text: data[i].text, date: data[i].date})}})}
+//        }                                  
+//      })
+//      this.socket.on('msg', (data) => {
+//        console.log(data);
+//       // this.insertTag(data);
+//      });
+//  }
+
+//  send(msg){
+//    console.log({user: "user", text: msg, date: new Date()})
+//    this.socket.emit('message', {user: "user", text: msg, date: 'new Date()'});
+//  }
+
+//   // insertTag(msg){
+//   //   console.log(msg);
+//   //   this.setState({arr:this.state.arr.concat(msg)})
+//   // }
+
+//  render(){
+//    return (
+//      <View>
+//        <Text>Hello in chat page</Text>
+//        <TextInput
+//            placeholder="Type here ..."
+//            onChangeText={(text) => {this.setState((pre)=> {return {msg:text}})}}  
+//            value={this.state.msg}/>
+//        <TouchableOpacity
+//            onPress={this.send(this.state.msg)}>
+//            <Text>Send</Text>
+//        </TouchableOpacity>
+//        <FlatList
+//          data={this.state.arr}
+//          renderItem={({item}) => <Text>{item.user + ': '+item.text}</Text>}
+//        />
+//      </View>
+//    )
+//  }
+// }
+
+
+
+// import React from 'react';
+// import { View, Text, TouchableOpacity, TextInput, Alert, FlatList, StyleSheet, } from 'react-native';
+// import SocketIOClient from 'socket.io-client';
+// // import SocketIOClient from 'https://code.jquery.com/jquery-1.11.1.js'
+// import { Icon } from 'react-native-elements'; 
+
+// // export default class App extends React.Component {
+// export default class ChatBox extends React.Component {
+//    static navigationOptions={
+//       header: null,    
+//     tabBarLabel:'ChatBox',
+//       tabBarIcon:()=> {
+//           return <Icon name="chat" size={25} color={"white"}/>
+//   }
+//   }
+// constructor(props){
+//   super(props);
+//   this.state={
+//     arr:[],
+//     msg: ''
+//   }
+
+//  this.send=this.send.bind(this);
+//  //  this.insertTag=this.insertTag.bind(this);
+
+//  this.socket = SocketIOClient('http://192.168.8.119:8000');
+//     this.socket.on('allDataBase', (data) => {
+//       for(var i=0; i<data.length; i++){
+//         // {this.setState((pre)=> {return {arr:this.state.arr.concat({user: data[i].user, text: data[i].text, date: data[i].date})}})}
+//          {this.setState((pre)=> {return {
+//             arr:pre.arr.concat({user: data[i].user, text: data[i].text, date: data[i].date})
+//         }})}
+//       }                                  
+//    })
+//     // this.socket.on('msg', (data) => {
+//     //   console.log(data);
+//     //  this.insertTag(data);
+//     // });
+// }
+
+// send(msg){
+//   console.log({user: "user", text: msg, date: new Date()})
+//   this.socket.emit('message', {user: "user", text: msg, date: 'new Date()'});
+// }
+
+// // insertTag(msg){
+// //    console.log(msg);
+// //    this.setState((pre) => {return {arr:pre.arr.concat(msg)}})
+// //  }
+
+// render(){
+//   return (
+//     <View>
+//       <Text>Hello in chat page</Text>
+//       <TextInput
+//           placeholder="Type here ..."
+//           onChangeText={(text) => 
+//             // if(text !== ''){
+//             {this.setState((pre)=> {return {msg:text}})}}  
+//           // }
+//          value={this.state.msg}/>
+//       <TouchableOpacity
+//           onPress={
+//             this.send(this.state.msg)
+//           }
+//           >
+//           <Text>Send</Text>
+//       </TouchableOpacity>
+//       <FlatList
+//         data={this.state.arr}
+//         renderItem={({item}) => <Text>{item.user + ': '+item.text}</Text>}
+//       />
+//     </View>
+//   )
+// }
+// }
